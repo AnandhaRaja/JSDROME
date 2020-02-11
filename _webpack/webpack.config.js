a9276fc29d2e340089eea8ab204212c70b65871e
Anandha Raja S S S <AnandhaRajaSSS@users.noreply.github.com>
2020-02-11 23:20:00 +0530

    HTML Webpack Plugin

--- _webpack/webpack.config.js
+++ _webpack/webpack.config.js
@@ -1,1 +1,8 @@
 module.exports = env => require(`./webpack.${env}.js`);
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+plugins: [
+  new HtmlWebpackPlugin({
+    title: 'My website',
+    template: path.resolve(__dirname, '../_templates/client.html'),
+  }),
+]
